2e9ad498c4211f0c601e4f802f8ef25362ac1f4e
parikshya098 <pbhandari1@ualr.edu>
2022-03-01 01:50:48 +0000

    Unit-6

--- 06.02 Stock.py
+++ 06.02 Stock.py
@@ -1,9 +1,10 @@
 def percentchange(todayprice, yesterdayprice):
-    return ((todayprice-yesterdayprice)/(yesterdayprice))*100
+    return ((todayprice - yesterdayprice) / (yesterdayprice)) * 100
 
 stockfile = open("06.02 Stock.txt","r")
-s = stockfile.read().split("\n")
+read = stockfile.read().split("\n")
 print("{0:>10s}{1:>10s}".format("Price","Change"))
-print("{0:>10s}{1:>10s}".format(s[0],""))
-for i in range(1,len(s)):
-    print("{0:>10s}{1:>10.2f}%".format(s[i],percentchange(float(s[i]),float(s[i-1]))))
+print("{0:>10s}{1:>10s}".format(read[0],""))
+
+for i in range(1, len(read)):
+    print("{0:>10s}{1:>10.2f}%".format(read[i],percentchange(float(read[i]),float(read[i-1]))))
